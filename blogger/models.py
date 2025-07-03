79ed7df847ebb5a1fa3c15232ac33b64720a0111
Alireza <golshandeveloper@gmail.com>
2025-07-03 22:04:26 +0330

    Initial commit - Django blog project

---
+++ blogger/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+class Blogger(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/bloggers/%Y/%m/%d/')
+    birth_date = models.DateTimeField(default=datetime.now , blank=True)
+    register_date = models.DateTimeField(default=datetime.now , blank=True)
+    posts_count = models.IntegerField(default=0)
+    description = models.TextField()
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
+
+

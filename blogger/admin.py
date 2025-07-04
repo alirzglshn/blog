d5a1d16f9de165ec7c36f2ad62b9dec007f09a46
Alireza <golshandeveloper@gmail.com>
2025-07-04 22:13:43 +0330

    4th july 2025 blog project

--- blogger/admin.py
+++ blogger/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Blogger
 # Register your models here.
-
-admin.site.register(Blogger)
+class BloggerAdmin(admin.ModelAdmin):
+    list_display = ('id' ,'name','photo','birth_date','register_date','posts_count','description','email','phone',)
+    list_display_links = [ 'id' , 'name' , 'email']
+    search_fields = ['name' , 'email' , 'phone']
+    readonly_fields = ['posts_count']
+    list_per_page = 10
+admin.site.register(Blogger , BloggerAdmin)
--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id' ,'title' ,'blogger' ,'comments_count','likes_count','post_date','is_published',)
+    list_display_links = [ 'id' , 'title']
+    list_editable = ['is_published']
+    list_filter = ['blogger']
+    search_fields = ['title' , 'text' , 'blogger__name']
+    readonly_fields = ['comments_count' , 'likes_count']
+    list_per_page = 20
+admin.site.register(Post , PostAdmin)

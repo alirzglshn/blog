7b30dd0c5a8b1e67be9b8d6da5909307ceff5939
Alireza <golshandeveloper@gmail.com>
2025-07-08 21:38:12 +0330

    9:37 pm july 8 2025 repo django blog

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,54 @@
 from django.shortcuts import render , redirect
 from django.contrib import messages
+from .forms import SignUpForm
+from django.contrib.auth import authenticate , login , logout
 # Create your views here.
 
 # sign in
 def sign_in(request):
-    if request.method == 'POST':
-        return redirect('sign_in')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+            messages.success(request, ("You have been logged in successfully"))
+            return redirect('index')
+        else:
+            messages.success(request, ("Login failed , Please try again."))
+            return redirect('sign_in')
     else:
         return render(request, 'accounts/sign_in.html', {})
 
+
 # sin up
 def sign_up(request):
+    form = SignUpForm()
     if request.method == 'POST':
-        messages.error(request , "this is a test for error message")
-        return redirect('sign_up')
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have been registered successfully")
+                return redirect("index")
+            else:
+                messages.error(request, "Registration succeeded, but login failed.")
+                return redirect("sign_in")
+        else:
+            messages.error(request, "Registration failed, please try again.")
+            return redirect("sign_up")
     else:
-        return render(request, 'accounts/sign_up.html', {})
+        return render(request, 'accounts/sign_up.html', {'form' : form})
 
 
 # sign out
 def sign_out(request):
-    if request.method == 'POST':
-        return redirect('sign_out')
-    else:
-        return render(request, 'accounts/sign_out.html', {})
+    logout(request)
+    messages.success(request, ("You have been signed out successfully"))
+    return redirect('index')
 

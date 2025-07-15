b225fb0f5444af9488323c0431053a5a2f68f42f
Alireza <golshandeveloper@gmail.com>
2025-07-15 21:18:01 +0330

    9:17 pm july 15 2025

--- comments/models.py
+++ comments/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from datetime import datetime
+from posts.models import Post
 # Create your models here.
 
 class Comment(models.Model):
     user_email = models.CharField(max_length=200)
     user_name = models.CharField(max_length=200)
     comment_date = models.DateTimeField(default= datetime.now ,blank=True)
-    post_id = models.IntegerField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
     comment_text = models.TextField()
 
 
--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render , get_object_or_404 , redirect
 from .models import Post
 from django.core.paginator import Paginator
 from django.db.models import Q
-
+from comments.models import Comment
+from comments.forms import The_Comments
 # Create your views here.
 def posts(request):
     posts_list = Post.objects.order_by('-post_date').filter(is_published = True)
@@ -17,7 +18,20 @@ def posts(request):
 
 def post(request, post_id:int):
     post = get_object_or_404(Post , pk=post_id)
-    return render(request , "posts/post.html" , {'post' : post})
+    comments = Comment.objects.filter(post=post)
+    form = The_Comments()
+
+    if request.method == 'POST':
+        form = The_Comments(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            post.comments_count += 1
+            post.save()
+            return redirect('post', post_id=post_id)
+
+    return render(request , "posts/post.html" , {'post' : post , 'comments' : comments , 'form' : form})
 
 def search(request):
     posts = Post.objects.order_by('-post_date').filter(is_published=True)

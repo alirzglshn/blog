f9a6e51f5d2e3fd81d10361e09332f74fc0e8b67
Alireza <golshandeveloper@gmail.com>
2025-07-20 22:52:06 +0330

    10:51 pm july 20 2025

--- user_messages/urls.py
+++ user_messages/urls.py
@@ -3,5 +3,7 @@ from user_messages import views
 
 urlpatterns = [
     path('send_message' , views.send_message , name = "send_message"),
+    path('inbox' , views.inbox , name = "inbox"),
+    path('message_history' , views.message_history , name = "message_history"),
 ]
 
--- user_messages/views.py
+++ user_messages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from .models import Message
 from user_messages.forms import MessageForm
+from django.db.models import Q
 
 
 # Create your views here.
@@ -13,7 +14,7 @@ def send_message(request):
             the_message = form.save(commit=False)
             the_message.sending_blogger = request.user
             the_message.save()
-            return redirect('bloggers_list')
+            return redirect('inbox')
     else :
         form = MessageForm()
     return render(request, 'send_message.html', {'form': form})
@@ -21,3 +22,11 @@ def send_message(request):
 def inbox(request):
     messages = Message.objects.filter(receiving_blogger=request.user).order_by('-time')
     return render(request, 'inbox.html', {'messages': messages})
+
+
+def message_history(request):
+    messages = Message.objects.filter(
+        Q(receiving_blogger=request.user) | Q(sending_blogger=request.user)
+    )
+    form = MessageForm()
+    return render(request , 'message_history.html' , {'messages' : messages , 'form' : form})
